9e6c7af73b9cbcf84eb2390d2de6f164b75d233e
Yevgeniy Zhitomirskiy <yevgeniy.zhitomirskiy@wework.com>
2018-02-19 01:29:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,14 +35,12 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets
+  input = gets.strip
+  index = input_to_index(input)
+  move(board, index, "X")
+  display_board(board)
 end
 
-# Code your CLI Here
-puts "Welcome to Tic Tac Toe!"
-board = [" "," "," "," "," "," "," "," "," "]
-puts "Where would you like to go?"
-input = gets.strip
-index = input_to_index(input)
-move(board, index, "X")
-display_board(board)
+
+
+
